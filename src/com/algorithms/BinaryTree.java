78e51f73b22a06850575f0a60d02b1af4dca5a2f
sangram kesari ray <shankar.ray030@gmail.com>
2014-10-14 02:54:13 +0530

    improved spacing

--- src/com/algorithms/BinaryTree.java
+++ src/com/algorithms/BinaryTree.java
@@ -6,11 +6,7 @@ public class BinaryTree {
 
     public void addNode(int key, String name) {
 
-        // Create a new Node and initialize it
-
         Node newNode = new Node(key, name);
-
-        // If there is no root this becomes root
 
         if (root == null) {
 
@@ -18,54 +14,34 @@ public class BinaryTree {
 
         } else {
 
-            // Set root as the Node we will start
-            // with as we traverse the tree
-
             Node focusNode = root;
-
-            // Future parent for our new Node
 
             Node parent;
 
             while (true) {
 
-                // root is the top parent so we start
-                // there
-
                 parent = focusNode;
-
-                // Check if the new node should go on
-                // the left side of the parent node
 
                 if (key < focusNode.key) {
 
-                    // Switch focus to the left child
-
                     focusNode = focusNode.leftChild;
-
-                    // If the left child has no children
 
                     if (focusNode == null) {
 
-                        // then place the new node on the left of it
-
                         parent.leftChild = newNode;
-                        return; // All Done
 
+                        return;
                     }
 
-                } else { // If we get here put the node on the right
+                } else {
 
                     focusNode = focusNode.rightChild;
 
-                    // If the right child has no children
-
                     if (focusNode == null) {
 
-                        // then place the new node on the right of it
-
                         parent.rightChild = newNode;
-                        return; // All Done
+
+                        return;
 
                     }
 
@@ -76,23 +52,13 @@ public class BinaryTree {
 
     }
 
-    // All nodes are visited in ascending order
-    // Recursion is used to go to one node and
-    // then go to its child nodes and so forth
-
     public void inOrderTraverseTree(Node focusNode) {
 
         if (focusNode != null) {
 
-            // Traverse the left node
-
         	inOrderTraverseTree(focusNode.leftChild);
 
-            // Visit the currently focused on node
-
             System.out.println(focusNode);
-
-            // Traverse the right node
 
             inOrderTraverseTree(focusNode.rightChild);
 
@@ -129,12 +95,15 @@ public class BinaryTree {
     }
     
     public void findMax(Node focusNode) {
+
         if (focusNode != null) {
 
-            //check if rightChild exist
             if (focusNode.rightChild == null) {
+
                 System.out.println(focusNode.key);
+
                 return;
+
             }
 
             Node newNode = focusNode.rightChild;
@@ -145,12 +114,15 @@ public class BinaryTree {
     }
     
     public void findMin(Node focusNode) {
+
     	if (focusNode != null) {
 
-            //check if leftChild exist
             if (focusNode.leftChild == null) {
+
                 System.out.println(focusNode.key);
+
                 return;
+
             }
 
             Node newNode = focusNode.leftChild;
@@ -183,139 +155,97 @@ public class BinaryTree {
 
     public boolean remove(int key) {
 
-        // Start at the top of the tree
-
         Node focusNode = root;
+
         Node parent = root;
 
-        // When searching for a com.algorithms.Node this will
-        // tell us whether to search to the
-        // right or left
-
         boolean isItALeftChild = true;
-
-        // While we haven't found the com.algorithms.Node
-        // keep looking
 
         while (focusNode.key != key) {
 
             parent = focusNode;
 
-            // If we should search to the left
-
             if (key < focusNode.key) {
 
                 isItALeftChild = true;
-
-                // Shift the focus com.algorithms.Node to the left child
 
                 focusNode = focusNode.leftChild;
 
             } else {
 
-                // Greater than focus node so go to the right
-
                 isItALeftChild = false;
-
-                // Shift the focus com.algorithms.Node to the right child
 
                 focusNode = focusNode.rightChild;
 
             }
-
-            // The node wasn't found
 
             if (focusNode == null)
                 return false;
 
         }
 
-        // If com.algorithms.Node doesn't have children delete it
-
         if (focusNode.leftChild == null && focusNode.rightChild == null) {
 
-            // If root delete it
-
             if (focusNode == root)
+
                 root = null;
 
-                // If it was marked as a left child
-                // of the parent delete it in its parent
-
             else if (isItALeftChild)
+
                 parent.leftChild = null;
 
-                // Vice versa for the right child
-
             else
+
                 parent.rightChild = null;
 
         }
 
-        // If no right child
-
         else if (focusNode.rightChild == null) {
 
             if (focusNode == root)
+
                 root = focusNode.leftChild;
 
-                // If focus com.algorithms.Node was on the left of parent
-                // move the focus Nodes left child up to the
-                // parent node
-
             else if (isItALeftChild)
+
                 parent.leftChild = focusNode.leftChild;
 
-                // Vice versa for the right child
-
             else
+
                 parent.rightChild = focusNode.leftChild;
 
         }
 
-        // If no left child
-
         else if (focusNode.leftChild == null) {
 
             if (focusNode == root)
+
                 root = focusNode.rightChild;
 
-                // If focus com.algorithms.Node was on the left of parent
-                // move the focus Nodes right child up to the
-                // parent node
-
             else if (isItALeftChild)
+
                 parent.leftChild = focusNode.rightChild;
 
-                // Vice versa for the left child
-
             else
+
                 parent.rightChild = focusNode.rightChild;
 
         }
-
-        // Two children so I need to find the deleted nodes
-        // replacement
 
         else {
 
             Node replacement = getReplacementNode(focusNode);
 
-            // If the focusNode is root replace root
-            // with the replacement
-
             if (focusNode == root)
+
                 root = replacement;
 
-                // If the deleted node was a left child
-                // make the replacement the left child
-
             else if (isItALeftChild)
+
                 parent.leftChild = replacement;
 
-                // Vice versa if it was a right child
-
             else
+
                 parent.rightChild = replacement;
 
             replacement.leftChild = focusNode.leftChild;
@@ -329,11 +259,10 @@ public class BinaryTree {
     public Node getReplacementNode(Node replacedNode) {
 
         Node replacementParent = replacedNode;
+
         Node replacement = replacedNode;
 
         Node focusNode = replacedNode.rightChild;
-
-        // While there are no more left children
 
         while (focusNode != null) {
 
@@ -345,14 +274,10 @@ public class BinaryTree {
 
         }
 
-        // If the replacement isn't the right child
-        // move the replacement into the parents
-        // leftChild slot and move the replaced nodes
-        // right child into the replacements rightChild
-
         if (replacement != replacedNode.rightChild) {
 
             replacementParent.leftChild = replacement.rightChild;
+
             replacement.rightChild = replacedNode.rightChild;
 
         }
@@ -365,14 +290,17 @@ public class BinaryTree {
 class Node {
 
     int key;
+
     String name;
 
     Node leftChild;
+
     Node rightChild;
 
     Node(int key, String name) {
 
         this.key = key;
+
         this.name = name;
 
     }
@@ -380,11 +308,6 @@ class Node {
     public String toString() {
 
         return name + " has the key " + key;
-
-		/*
-		 * return name + " has the key " + key + "\nLeft Child: " + leftChild +
-		 * "\nRight Child: " + rightChild + "\n";
-		 */
 
     }
 
